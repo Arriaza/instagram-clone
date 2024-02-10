400ad89f213dfa7d0aa9f7148b5bf33d12f1df10
Joshua Arriaza <arriazaandre@gmail.com>
2024-02-10 13:29:43 -0800

    Create comment

--- src/components/Profile/ProfilePost.js
+++ src/components/Profile/ProfilePost.js
@@ -134,13 +134,14 @@ const ProfilePost = ({ post }) => {
                 <Divider my={4} bg={"gray.500"} />
 
                 <VStack w="full" alignItems={"start"} maxH={"350px"} overflowY={"auto"}>
-                  <Comment createdAt="1d ago" username="asaprogrammer" profilePic="/profilepic.png" text={"Dummy images from unsplash"}  />
-                  <Comment createdAt="12h ago" username="AbrahMov" profilePic={"https://bit.ly/dan-abramov"} text={"Nice pic"}  />
+                  {post.comments.map(comment => (
+                    <Comment key={comment.id} comment={comment}/>
+                  ))}
                 </VStack>
 
                 <Divider my={4} bg={"gray.800"} />
 
-                <PostFooter />
+                <PostFooter isProfilePage={true} post={post} />
 
               </Flex>
 
