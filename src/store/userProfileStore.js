2db19f2b5a15840866d2199eb00d12a82f4be86e
Joshua Arriaza <arriazaandre@gmail.com>
2024-02-10 11:39:28 -0800

    Delete post

--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -6,7 +6,10 @@ const useUserProfileStore = create(set => ({
   // this is used to update the number of posts in the profile page
   addPost: post => set(state => ({
     userProfile: { ...state.userProfile, posts: [post.id, ...state.userProfile.posts] }
-  }))
+  })),
+  deletePost: postId => set(state => ({
+    userProfile: { ...state.userProfile, posts: state.userProfile.posts.filter(id => id !== postId) },
+  })),
 
   }))
 
