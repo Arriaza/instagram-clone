347eabe7cf739a0436e711a32dc5f44d44e74d88
Joshua Arriaza <arriazaandre@gmail.com>
2024-02-12 18:02:00 -0800

    Updating route AuthPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
-import AuthPage from "./AuthPage/AuthPage"
+import AuthPage from "./pages/AuthPage/AuthPage";
 import PageLayout from "./Layouts/PageLayout/PageLayout";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import { useAuthState } from "react-firebase-hooks/auth";
